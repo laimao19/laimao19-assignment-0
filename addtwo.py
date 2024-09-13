21987af0e85a446c69438a27a27c21e1c0c8c3a1
laimao19 <laimao19@bu.edu>
2024-09-13 11:07:53 -0400

    implemented the addding two numbers together and printing out the result script

---
+++ addtwo.py
@@ -0,0 +1,12 @@
+def add_two_num(num1, num2):
+    return num1 + num2
+
+#set two arbitrary numbers
+num1 = 100
+num2 = 15678
+
+#calculate the sum
+sum_result = add_two_num(num1, num2)
+
+#otput: print the sum
+print(f"The sum of {num1} and {num2} is: {sum_result}")
